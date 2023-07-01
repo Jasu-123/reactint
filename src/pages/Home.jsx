a02f384927d5c991fe2ac6a5a4780a09b7c1818a
jasu-123 <anushajasu9961@gmail.com>
2023-07-01 11:20:48 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import music from './'
+import { Login } from './Login'
+import music from "../assets"
+
+export const Home = () => {
+    return (
+        <>
+            <div className="nav">
+                <div className='image'>
+                    img src={music}
+                </div>
+                <div className="s">Spotify</div>
+                <div className="P">Premium</div>
+                <div className="H">Help</div>
+                <div className="D">Download</div>
+                <link to={"/registration"}>
+                    <div className="s">sign up</div>
+                </link>
+                <link to={"/login"}>
+                    <div className="l">login</div>
+                </link>
+
+            </div >
+        </>
+    )
+}
+
